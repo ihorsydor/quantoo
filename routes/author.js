3525eac2e993cde1d4073e4bea5cb98c4f30e520
ihor <ihor@poczta.onet.pl>
2023-06-26 14:30:06 +0200

    search backend działa

--- routes/author.js
+++ routes/author.js
@@ -3,12 +3,23 @@ var authorController = require('../controllers/author.controller')
 var router = express.Router();
 
 /* GET users listing. */
+// router.get("/:a", function(req,res,next){
+//     res.send('TEST');
+// });
+// router.get('/account/abc', function(req, res) {
+//     res.send('TEST');
+//     console.log('TEST', req);
+  
+//       res.status(200)
+    
+//   });
+router.get('/search', authorController.searchAuthor);
 router.get('/', authorController.getAllAuthor);
 router.get('/:id', authorController.getOneAuthor);
 router.post('/', authorController.createAuthor);
 router.put('/:id', authorController.updateAuthor);
 router.delete('/:id', authorController.deleteAuthor);
-router.get('/:id/search', authorController.searchAuthor);
+
 
 
 
