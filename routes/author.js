b99d6b1b0e81ddb70f9a21c0988020fab4d78626
ihor <ihor@poczta.onet.pl>
2023-06-26 10:52:51 +0200

    aktualizacja

--- routes/author.js
+++ routes/author.js
@@ -3,21 +3,12 @@ var authorController = require('../controllers/author.controller')
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req,res,next){
-    res.send('TEST');
-});
-router.get('/account/abc', function(req, res) {
-    console.log('TEST', req);
-  
-      res.status(200)
-    
-  });
 router.get('/', authorController.getAllAuthor);
 router.get('/:id', authorController.getOneAuthor);
 router.post('/', authorController.createAuthor);
 router.put('/:id', authorController.updateAuthor);
 router.delete('/:id', authorController.deleteAuthor);
-router.get('/search', authorController.searchAuthor);
+router.get('/:id/search', authorController.searchAuthor);
 
 
 
